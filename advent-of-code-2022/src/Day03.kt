0b9414a2ee7fd5277ae58cca6a6a164b1cdb1bb8
Rok Koleša <rok.kolesa@medius.si>
2022-12-03 12:27:28 +0100

    Add 2022 Day03 solution.

---
+++ advent-of-code-2022/src/Day03.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val charMap = buildMap {
+        putAll("abcdefghijklmnopqrstuvwxyz".toCharArray().associateWith { it.code - 96 })
+        putAll("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray().associateWith { it.code - 64 + 26 })
+    }
+
+    fun part1(input: List<String>): Int = input
+        .map { listOf(it.substring(0, it.length / 2), it.substring(it.length / 2)) }
+        .map { it.map(String::toCharArray) }
+        .map { (firstChars, secondChars) -> firstChars.find { it in secondChars } }
+        .sumOf { charMap[it]!! }
+
+    fun part2(input: List<String>): Int = input
+        .windowed(size = 3, step = 3)
+        .map { it.map(String::toCharArray) }
+        .map { (firstChars, secondChars, thirdChars) -> firstChars.find { it in secondChars && it in thirdChars }!! }
+        .sumOf { charMap[it]!! }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
