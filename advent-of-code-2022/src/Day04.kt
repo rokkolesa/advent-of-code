6c53eb2e4c657fa570961ef92c0bccb6fd14d5d6
Rok Koleša <rok.kolesa@medius.si>
2022-12-04 11:10:54 +0100

    Add 2022 Day04 solution.

---
+++ advent-of-code-2022/src/Day04.kt
@@ -0,0 +1,36 @@
+fun main() {
+    infix fun IntRange.contains(other: IntRange): Boolean =
+        this.contains(other.first) && this.contains(other.last)
+
+    infix fun IntRange.intersects(other: IntRange): Boolean =
+        this.contains(other.first) || this.contains(other.last)
+
+    val inputLineRegex = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
+
+    fun parseInput(line: String): List<IntRange> {
+        val (firstFrom, firstTo, secondFrom, secondTo) = inputLineRegex
+            .matchEntire(line)
+            ?.destructured
+            ?.toList()
+            ?.map(String::toInt)
+            ?: throw IllegalArgumentException("Incorrect input line $line")
+        return listOf(firstFrom..firstTo, secondFrom..secondTo)
+    }
+
+    fun part1(input: List<String>): Int = input
+        .map(::parseInput)
+        .count { (first, second) -> first contains second || second contains first }
+
+    fun part2(input: List<String>): Int = input
+        .map(::parseInput)
+        .count { (first, second) -> first intersects second || second intersects first }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
