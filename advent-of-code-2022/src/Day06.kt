e41f6a9cb3bbd26b0dcccd5c7ea167d1c94b10f8
Rok Koleša <rok.kolesa@medius.si>
2022-12-06 08:01:25 +0100

    Add 2022 Day06 solution.

---
+++ advent-of-code-2022/src/Day06.kt
@@ -0,0 +1,29 @@
+fun main() {
+
+    fun distinctChars(input: String, numOfChars: Int): Int =
+        input.asSequence()
+            .windowed(numOfChars)
+            .map(List<Char>::toSet)
+            .indexOfFirst { it.size == numOfChars } + numOfChars
+
+    fun part1(input: String): Int = distinctChars(input, 4)
+    fun part2(input: String): Int = distinctChars(input, 14)
+
+    // test if implementation meets criteria from the description, like:
+    check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7)
+    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
+    check(part1("nppdvjthqldpwncqszvftbrmjlhg") == 6)
+    check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10)
+    check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11)
+
+    check(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19)
+    check(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23)
+    check(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
+    check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+    check(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
+
+
+    val input = readInput("Day06").first()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
