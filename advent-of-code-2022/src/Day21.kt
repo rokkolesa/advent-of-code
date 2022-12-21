f8d08b8fe5d438727d7ab30846e46ff9f5738069
Rok Koleša <rok.kolesa@medius.si>
2022-12-21 23:21:57 +0100

    [chore] Refactor 2022 Day21.

--- advent-of-code-2022/src/Day21.kt
+++ advent-of-code-2022/src/Day21.kt
@@ -4,11 +4,15 @@ import utils.*
 import kotlin.IllegalStateException
 
 typealias Num = Long
+typealias Operations = List<Operation>
 
-class Operation(val name: String, val constant: Boolean = false, val op: (Num, Num) -> Num) {
+class Operation(private val name: String, val constant: Boolean = false, val op: (Num, Num) -> Num) {
     lateinit var leftInv: Operation
     lateinit var rightInv: Operation
 
+    fun rightPartial(left: Num) = Operation(rightInv.name) { _, r -> rightInv(left, r) }
+    fun leftPartial(right: Num) = Operation(leftInv.name) { l, _ -> leftInv(l, right) }
+    fun singleton() = listOf(this)
     operator fun invoke(left: Num, right: Num): Num = op(left, right)
     operator fun invoke(coeff: Num) = invoke(coeff, coeff)
     operator fun invoke() = invoke(0, 0)
@@ -18,37 +22,50 @@ class Operation(val name: String, val constant: Boolean = false, val op: (Num, N
 
 data class Monkey(val name: String, val operation: Operation, val subMonkeys: List<String>)
 
-fun List<Operation>.toConstant() = this[0]()
-fun List<Operation>.isConstant() = all(Operation::constant)
+fun Operations.toConstant() = this[0]()
+fun Operations.isConstant() = all(Operation::constant)
+fun Num.toOperation() = Operation("", constant = true) { _, _ -> this }.apply { leftInv = this; rightInv = this }
 
 fun main() {
-    val plusOperation = Operation("+") { left, right -> left + right }
-        .apply {
-            leftInv = Operation("-") { left, right -> left - right }
-            rightInv = Operation("-") { left, right -> right - left }
-        }
+    val plusOperation =
+        // a + b = c
+        Operation("+") { left, right -> left + right }
+            .apply {
+                // a = b - c
+                leftInv = Operation("-") { left, right -> left - right }
+                // b = c - a
+                rightInv = Operation("-") { left, right -> right - left }
+            }
 
-    val minusOperation = Operation("-") { left, right -> left - right }
-        .apply {
-            leftInv = Operation("+") { left, right -> left + right }
-            rightInv = Operation("-") { left, right -> left - right }
-        }
+    val minusOperation =
+        // a - b = c
+        Operation("-") { left, right -> left - right }
+            .apply {
+                // a = c + b
+                leftInv = Operation("+") { left, right -> left + right }
+                // b = a - c
+                rightInv = Operation("-") { left, right -> left - right }
+            }
 
     val multiplyOperation =
+        // a * b = c
         Operation("*") { left, right -> left * right }
             .apply {
+                // a = c / b
                 leftInv = Operation("/") { left, right -> left / right }
+                // b = c / a
                 rightInv = Operation("/") { left, right -> right / left }
             }
 
     val divisionOperation =
+        // a / b = c
         Operation("/") { left, right -> left / right }
             .apply {
+                // a = c * b
                 leftInv = Operation("*") { left, right -> left * right }
+                // b = c / a
                 rightInv = Operation("/") { left, right -> right / left }
-
             }
-    val constantOperation = { l: Num -> Operation("", constant = true) { _, _ -> l }.apply { leftInv = this; rightInv = this } }
 
     fun parseMonkeys(input: List<String>): List<Monkey> = input.map {
         val (name, operationDefinition) = it.split(": ")
@@ -57,7 +74,7 @@ fun main() {
             operationDefinition.contains("-") -> operationDefinition.split(" - ").let { (left, right) -> minusOperation to listOf(left, right) }
             operationDefinition.contains("*") -> operationDefinition.split(" * ").let { (left, right) -> multiplyOperation to listOf(left, right) }
             operationDefinition.contains("/") -> operationDefinition.split(" / ").let { (left, right) -> divisionOperation to listOf(left, right) }
-            else -> constantOperation(operationDefinition.toLong()) to listOf()
+            else -> operationDefinition.toLong().toOperation() to listOf()
         }
 
         Monkey(name, operation, subMonkeys)
@@ -89,69 +106,61 @@ fun main() {
 
     fun part2(input: List<String>): Num = parseMonkeys(input).associateBy(Monkey::name)
         .let { monkeys ->
-            val cache = mutableMapOf<String, List<Operation>>()
+            val cache = mutableMapOf<String, Operations>()
 
-
-            fun findOperation(monkey: String): List<Operation> {
+            fun findOperations(monkey: String): Operations {
                 val queue = ArrayDeque<String>()
                 queue.addFirst(monkey)
                 while (queue.isNotEmpty()) {
                     val monkeyName = queue.removeFirst()
+                    if (cache[monkeyName] != null) continue
+
                     val current = monkeys[monkeyName]!!
                     val subMonkeys = current.subMonkeys
 
-                    if (subMonkeys.isEmpty()) {
-                        cache[monkeyName] = listOf(constantOperation(current.operation()))
-                    } else if (subMonkeys.all { it in cache }) {
-                        val leftOperations = cache[subMonkeys.first()]!!
-                        val rightOperations = cache[subMonkeys.last()]!!
-
-                        if (leftOperations.isConstant() && rightOperations.isConstant()) {
-                            cache[monkeyName] = listOf(constantOperation(current.operation(leftOperations.toConstant(), rightOperations.toConstant())))
-                        } else if (leftOperations.isConstant()) {
-                            cache[monkeyName] = rightOperations + Operation(current.operation.name) { _, r ->
-                                current.operation.rightInv(leftOperations.toConstant(), r)
-                            }
-
-                        } else if (rightOperations.isConstant()) {
-                            cache[monkeyName] = leftOperations + Operation(current.operation.name) { l, _ ->
-                                current.operation.leftInv(l, rightOperations.toConstant())
-                            }
-                        } else {
-                            throw IllegalStateException()
-                        }
-                    } else if (subMonkeys.any { it == "humn" } && subMonkeys.find { it != "humn" } in cache) {
-                        val (left, right) = subMonkeys
-                        if (left == "humn") {
-                            val rightOperations = cache[right]!!
-                            cache[monkeyName] = listOf(Operation(current.operation.name) { l, _ ->
-                                current.operation.leftInv(l, rightOperations.toConstant())
-                            })
-
-                        } else {
-                            val leftOperations = cache[left]!!
-                            cache[monkeyName] = listOf(Operation(current.operation.name) { _, r ->
-                                current.operation.rightInv(leftOperations.toConstant(), r)
-                            })
+                    when {
+                        subMonkeys.isEmpty() -> cache[monkeyName] = current.operation().toOperation().singleton()
+                        subMonkeys.all { it in cache } -> {
+                            cache[monkeyName] = subMonkeys.map { cache[it]!! }
+                                .let { (leftOperations, rightOperations) ->
+                                    when {
+                                        leftOperations.isConstant() && rightOperations.isConstant() -> current.operation(leftOperations.toConstant(), rightOperations.toConstant()).toOperation().singleton()
+                                        leftOperations.isConstant() -> rightOperations + current.operation.rightPartial(leftOperations.toConstant())
+                                        rightOperations.isConstant() -> leftOperations + current.operation.leftPartial(rightOperations.toConstant())
+                                        else -> throw IllegalStateException()
+                                    }
+                                }
                         }
 
-                    } else {
-                        subMonkeys.filter { it !in cache }.filter { it != "humn" }.forEach { queue.addFirst(it) }
-                        queue.addLast(monkeyName)
+                        subMonkeys.any { it == "humn" } && subMonkeys.find { it != "humn" } in cache -> {
+                            cache[monkeyName] = subMonkeys
+                                .let { (left, right) ->
+                                    when (left) {
+                                        "humn" -> current.operation.leftPartial(cache[right]!!.toConstant()).singleton()
+                                        else -> current.operation.rightPartial(cache[left]!!.toConstant()).singleton()
+                                    }
+                                }
+                        }
+
+                        else -> {
+                            subMonkeys.filter { it !in cache }.filter { it != "humn" }.forEach { queue.addFirst(it) }
+                            queue.addLast(monkeyName)
+                        }
                     }
                 }
 
                 return cache[monkey]!!
             }
-            val (leftMonkey, rightMonkey) = monkeys["root"]!!.subMonkeys
-            val leftOperation = findOperation(leftMonkey)
-            val rightOperation = findOperation(rightMonkey)
 
-            val (function, constant) =
-                if (leftOperation.isConstant()) rightOperation to leftOperation.toConstant()
-                else leftOperation to rightOperation.toConstant()
-
-            return function.foldRight(constant) { operation, acc -> operation(acc) }
+            monkeys["root"]!!.subMonkeys.map(::findOperations)
+                .let { (leftOperation, rightOperation) ->
+                    when {
+                        leftOperation.isConstant() -> rightOperation to leftOperation.toConstant()
+                        rightOperation.isConstant() -> leftOperation to rightOperation.toConstant()
+                        else -> throw IllegalStateException()
+                    }
+                }
+                .let { (operations, constant) -> operations.foldRight(constant) { operation, acc -> operation(acc) } }
         }
 
     // test if implementation meets criteria from the description, like:
