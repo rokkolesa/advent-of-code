d219f1ad39bc9e4235501e638b52165831cb6cfd
Rok Koleša <rok.kolesa@gmail.com>
2023-12-15 07:43:16 +0100

    feat: add 2023 Day15 solution

---
+++ advent-of-code-2023/src/day15/day15.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"../shared"
+	_ "embed"
+	"slices"
+	"strings"
+)
+
+//go:embed day15_sample.txt
+var sample string
+
+//go:embed day15.txt
+var input string
+
+func part1(input string) int {
+	return shared.Reduce(
+		strings.Split(input, ","),
+		0,
+		func(sum int, step string) int { return sum + hash(step) },
+	)
+}
+
+func hash(step string) int {
+	hash := 0
+	for _, character := range step {
+		hash += int(character)
+		hash *= 17
+		hash = hash % 256
+	}
+	return hash
+}
+
+type Lens struct {
+	label string
+	value int
+}
+
+func part2(input string) (sum int) {
+	boxes := make(map[int][]*Lens)
+	for _, step := range strings.Split(input, ",") {
+		if strings.Contains(step, "=") {
+			stepSplit := strings.Split(step, "=")
+			label := stepSplit[0]
+			value := shared.ParseIntSafe(stepSplit[1])
+			labelHash := hash(label)
+
+			// if the box exists and the label does not => append the Lens
+			// if the box exists and the label exists => update the value of the existing Lens
+			// if the box does not exist => add the Lens to the box
+			if box, exists := boxes[labelHash]; exists {
+				if lensIndex := slices.IndexFunc(box, func(lens *Lens) bool { return lens.label == label }); lensIndex >= 0 {
+					box[lensIndex].value = value
+				} else {
+					boxes[labelHash] = append(box, &Lens{label: label, value: value})
+				}
+			} else {
+				boxes[labelHash] = []*Lens{{label: label, value: value}}
+			}
+		} else {
+			label := step[:len(step)-1]
+			labelHash := hash(label)
+			if box, exists := boxes[labelHash]; exists {
+				boxes[labelHash] = slices.DeleteFunc(box, func(lens *Lens) bool { return lens.label == label })
+			}
+		}
+	}
+	for i, box := range boxes {
+		for j, lens := range box {
+			sum += (i + 1) * (j + 1) * lens.value
+		}
+	}
+	return
+}
+
+func main() {
+	shared.Check("Part 1", 1320, func() int {
+		return part1(sample)
+	})
+	shared.Check("Part 1", 510792, func() int {
+		return part1(input)
+	})
+	shared.Check("Part 2", 145, func() int {
+		return part2(sample)
+	})
+	shared.Check("Part 2", 269410, func() int {
+		return part2(input)
+	})
+}
